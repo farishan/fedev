aba7e36214770003b538bb1077ac24cff6063795
Faris Hanugraha <farishanugraha@gmail.com>
2020-05-14 00:24:22 +0700

    Updates

---
+++ precache-manifest.0975c30f74a608d30869da032139f81a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3d5be4715b6d983f6bf78e7e9c9949",
+    "url": "/fedev/index.html"
+  },
+  {
+    "revision": "472422066e174fc087e7",
+    "url": "/fedev/static/css/main.988dfa8b.chunk.css"
+  },
+  {
+    "revision": "8e9d6736117c91871ecb",
+    "url": "/fedev/static/js/2.4700a9b5.chunk.js"
+  },
+  {
+    "revision": "ab6bd92c46b489bf0abe330868a5708e",
+    "url": "/fedev/static/js/2.4700a9b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472422066e174fc087e7",
+    "url": "/fedev/static/js/main.81688b79.chunk.js"
+  },
+  {
+    "revision": "3cbd9e1cb99d94822cab",
+    "url": "/fedev/static/js/runtime-main.41d5f9c6.js"
+  }
+]);
